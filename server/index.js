b60a50a0470655f74f9c92cbfaa091476c4d7065
22Pratiksha <pratikshashelar38@gmail.com>
2025-05-06 19:00:44 +0530

    Fix .env file - Remove spaces in backend URL

--- server/index.js
+++ server/index.js
@@ -9,51 +9,54 @@ const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const {cloudinaryConnect } = require("./config/cloudinary");
+const { cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-//database connect
+// Connect to database
 database.connect();
-//middlewares
+
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
+		origin: [
+			"http://localhost:3000",
+			"https://online-education-platform-theta.vercel.app"
+		],
+		credentials: true,
 	})
-)
+);
 
 app.use(
 	fileUpload({
-		useTempFiles:true,
-		tempFileDir:"/tmp",
+		useTempFiles: true,
+		tempFileDir: "/tmp",
 	})
-)
-//cloudinary connection
+);
+
+// Connect to cloudinary
 cloudinaryConnect();
 
-//routes
+// Routes
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
 
-//def route
-
+// Default route
 app.get("/", (req, res) => {
 	return res.json({
-		success:true,
-		message:'Your server is up and running....'
+		success: true,
+		message: 'Your server is up and running....'
 	});
 });
 
 app.listen(PORT, () => {
-	console.log(`App is running at ${PORT}`)
-})
-
+	console.log(`App is running at ${PORT}`);
+});
